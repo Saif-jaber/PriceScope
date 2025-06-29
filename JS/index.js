5e1a06bcb8510b57bd43cfe693c59cd5b47fc8c9
Saif-jaber <saif.jaber.abed@gmail.com>
2025-06-30 00:14:29 +0400

    v4 version + landing page completed but no local storage, policies page completed

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,11 @@
-function goToPage(){
-    window.location.href = "TermsAndPolicies.html"
+function goToPageForPrivacypolicy(){
+    window.location.href = "TermsAndPolicies.html#privacy-policy"
+}
+
+function goToPageForTerms(){
+    window.location.href = "TermsAndPolicies.html#terms"
+}
+
+function goToPageForCodeOfConduct(){
+    window.location.href = "TermsAndPolicies.html#Conduct"
 }
