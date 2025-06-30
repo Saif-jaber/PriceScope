f0de812a38295bd26b7d3374084aba46fc52ba69
Saif-jaber <saif.jaber.abed@gmail.com>
2025-06-30 18:56:40 +0400

    v5 of the files + the search local storage is done

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,8 @@
+const searchedInput = document.querySelector("#search-bar")
+const searchContainer = document.querySelector("#search-container")
+const messageBox = document.querySelector('#message-box')
+let isEmpty = 0; // 1 = is not empty , 0 = empty
+
 function goToPageForPrivacypolicy(){
     window.location.href = "TermsAndPolicies.html#privacy-policy"
 }
@@ -9,3 +14,29 @@ function goToPageForTerms(){
 function goToPageForCodeOfConduct(){
     window.location.href = "TermsAndPolicies.html#Conduct"
 }
+
+searchedInput.addEventListener("input", () => {
+    if (searchedInput.value == ""){
+        isEmpty = 0;
+        searchContainer.style.borderColor = "red";
+        messageBox.textContent = "Please enter a product name to start searching"
+    }
+
+    else{
+        isEmpty = 1;
+        searchContainer.style.borderColor = "white"; 
+        messageBox.textContent = ""
+    }
+})
+
+function saveAndSearch(){
+    if(isEmpty == 1){
+        localStorage.setItem("searchedItem", searchedInput.value.trim())
+        window.location.href = "TermsAndPolicies.html"
+    }
+    else{
+        searchContainer.style.borderColor = "red";
+        messageBox.textContent = "Please enter a product name to start searching"
+    }
+}
+
